f6a39112f3a516563d92b88e94cf35c2cc5e7e2d
kimuyu.cocoa <kimu.cocoa@icloud.com>
2019-03-21 14:05:50 +0000

    add colum

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
-    validates :content, presence: true, length: { maxmum: 255 }
-    validates :status, presence: ture, length: { maxmum: 255 }
+    validates :content, presence: true, length: { maximum: 255 }
+  validates :status, presence: true, length: { maximum: 255 }
 end
