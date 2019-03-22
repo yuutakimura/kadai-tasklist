4a3300140e82d5bca2093d77b38ae2fa7b2ad8f5
kimuyu.cocoa <kimu.cocoa@icloud.com>
2019-03-22 15:22:26 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,60 +1,56 @@
 class TasksController < ApplicationController
-  
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
-    @tasks = Tasks.all
-  end 
-  
+    @tasks = Task.all.page(params[:page])
+  end
+
   def show
-    set_tasks
-  end 
-  
-  def new 
-    @tasks = Tasks.new
-  end 
-  
+  end
+
+  def new
+    @task = Task.new
+  end
+
   def create
-    @tasks = Tasks.new(tasks_params)
-    
-    if @tasks.save
-      flash[:success] = 'Tasks が正常に登録されました'
-      redirect_to @tasks
+    @task = Task.new(task_params)
+
+    if @task.save
+      flash[:success] = 'Task が正常に投稿されました'
+      redirect_to @task
     else
-      flash.now[:danger] = 'Tasks が登録されませんでした'
-      render:new
+      flash.now[:danger] = 'Task が投稿されませんでした'
+      render :new
     end
   end
-  
-  def edit
-    set_tasks
-  end 
-  
-  def update
-    set_tasks
-    if @tasks.update(tasks_params)
-      flash[:success] = 'Tasks は更新できました'
-      redirect_to @tasks
-    else
-      flash.now[:danger] = 'Tasks は更新できませんでした'
-      render:edit
-    end
-  end 
-  
-  def destroy
-    set_tasks
-    @task.destroy
-    
-    flash[:success] ='Tasks は削除しました'
-    redirect_to tasks_url
-  end 
 
-  
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task は更新されませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
+  end
+
   private
-  #Strong Parameter
-  
-  def set_tasks
-    @tasks = Tasks.find(params[:id])
-  end 
-  def tasks_params
-    params.require(:tasks).permit(:content)
-  end  
-end 
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
+end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
-class Tasks < ApplicationRecord
+class Task < ApplicationRecord
   validates :content, presence: true, length: { maximum: 255 }
   validates :status, presence: true, length: { maximum: 10 }
+  
 end
