6db4cf3b1d1ecea94ac1c8d22e30dce3e0d35525
kimuyu.cocoa <kimu.cocoa@icloud.com>
2019-03-24 13:24:23 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   
   root to: "tasks#index"
+  
+   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
  end
