4813e6623ea9120c8ab470edc55b474a22ff41c4
kimuyu.cocoa <kimu.cocoa@icloud.com>
2019-03-28 06:59:32 +0000

    bootstrap design

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
   
   root to: "tasks#index"
-  
-   get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
-
-  
-  get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
-  
-  resources :microposts, only: [:create, :destoy]
- end
+  resources :tasks,only: [:index, :show, :new, :create]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
--- db/migrate/20190319175615_create_tasks.rb
+++ db/migrate/20190319175615_create_tasks.rb
@@ -2,6 +2,9 @@ class CreateTasks < ActiveRecord::Migration[5.0]
   def change
     create_table :tasks do |t|
       t.string :content
+      t.string :name
+      t.string :email
+      t.string :password_digest
 
       t.timestamps
     end
