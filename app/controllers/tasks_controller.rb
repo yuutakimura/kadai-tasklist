ed88e3d8bc91b5ce9bbb46a74ae5c38923ed801b
kimuyu.cocoa <kimu.cocoa@icloud.com>
2019-03-31 12:53:17 +0000

    修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,14 +15,13 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
-
-    if @task.save
+     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
       redirect_to @task
     else
       flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
-    end
+     end
   end
 
   def edit
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,5 @@
 class Task < ApplicationRecord
   validates :content, presence: true, length: { maximum: 255 }
   validates :status, presence: true, length: { maximum: 10 }
- before_save { self.email.downcase! }
-  validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
-                    uniqueness: { case_sensitive: false }
-  has_secure_password
+
 end
