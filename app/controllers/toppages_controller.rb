695bd54e8cd482c25170cbd98a80ce34f6724540
kimuyu.cocoa <kimu.cocoa@icloud.com>
2019-04-12 13:13:33 +0000

    post

--- app/controllers/toppages_controller.rb
+++
@@ -1,8 +0,0 @@
-class ToppagesController < ApplicationController
-  def index
-    if logged_in?
-      @micropost = current_user.microposts.build  # form_for 用
-      @microposts = current_user.microposts.order('created_at DESC').page(params[:page])
-    end 
-  end
-end
--- db/migrate/20190321124347_add_status_totasks.rb
+++
@@ -1,5 +0,0 @@
-class AddStatusTotasks < ActiveRecord::Migration[5.0]
-  def change
-    add_column :tasks, :status, :string
-  end
-end
